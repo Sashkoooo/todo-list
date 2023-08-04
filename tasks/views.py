ffeba0e8a3fb1b6de4cf1834a175dbd675021e3f
Sashkoooo <o.denysenko@hotmail.com>
2023-08-04 19:46:30 +0300

    Implement project`s views

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+from tasks.models import Task, Tag
+
+
+class TaskListView(generic.ListView):
+    model = Task
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("tasks:task-list")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("tasks:task-list")
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    # template_name = "task_confirm_delete.html"
+    success_url = reverse_lazy("tasks:task-list")
+
+
+class TagListView(generic.ListView):
+    model = Tag
+    # template_name = "tag_list.html"
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("tasks:tag-list")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("tasks:tag-list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    # template_name = "tag_confirm_delete.html"
+    success_url = reverse_lazy("tasks:tag-list")
