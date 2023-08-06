59d13ea0bb0cafe46db9967ad8e1edc8b540ff51
Sashkoooo <o.denysenko@hotmail.com>
2023-08-06 14:15:58 +0300

    Add toggle_task view

--- tasks/urls.py
+++ tasks/urls.py
@@ -8,15 +8,19 @@ from tasks.views import (
     TagDeleteView,
     TagListView,
     TagUpdateView,
+    toggle_task,
 )
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="task-list"),
-    path("/create/", TaskCreateView.as_view(), name="task-create"),
-    path("/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
-    path("/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
+    path("create/", TaskCreateView.as_view(), name="task-create"),
+    path("<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
+    path("<int:pk>/toggle-task/", toggle_task, name="toggle-task"),
     path("tags/", TagListView.as_view(), name="tag-list"),
     path("tags/create/", TagCreateView.as_view(), name="tag-create"),
     path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
     path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
 ]
+
+app_name = "tasks"
